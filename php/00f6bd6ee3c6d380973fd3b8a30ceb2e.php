c9acede9d3a7edd2559a1d2cc33550b2548938cc
videsample <semencha.aleksey@gmail.com>
2023-04-24 20:25:45 +0300

    Add white1

---
+++ php/00f6bd6ee3c6d380973fd3b8a30ceb2e.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+    <!-- site metas -->
+     
+    <title>Статья</title>
+
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css" />
+    <!-- Responsive-->
+    <link rel="stylesheet" href="css/responsive.css" />
+    <!-- fevicon -->
+    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css" />
+    <!-- Tweaks for older IEs-->
+    <link
+      rel="stylesheet"
+      href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
+      media="screen"
+    />
+  </head>
+  <!-- body -->
+  <body class="main-layout">
+    <!-- loader  -->
+    <div class="loader_bg">
+      <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+    </div>
+    <!-- end loader -->
+    <!-- header -->
+    <header>
+      <!-- header inner -->
+      <div class="head_top">
+        <div class="container">
+          <div class="header">
+            <div class="row" style="align-items: center">
+              <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                <div class="full">
+                  <div class="center-desk">
+                    <div class="logo">
+                      <a href="index.php">Мой вклад</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                <nav class="navigation navbar navbar-expand-md navbar-dark">
+                  <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarsExample04"
+                    aria-controls="navbarsExample04"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarsExample04">
+                    <ul class="navbar-nav mr-auto" style="flex-wrap: wrap">
+                      <li class="nav-item">
+                        <a class="nav-link" href="index.php">Главная</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="blog.php">Статьи</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Контакты</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="policy.php">Политика конфиденциальности</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="terms.php">Условия и положения</a>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end header inner -->
+        <!-- end header -->
+        <!-- banner -->
+        <section class="banner_main">
+          <div class="container">
+            <div class="row d_flex">
+              <div class="col-md-12">
+                <div class="text-bg">
+                  <h1>Статья</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </header>
+    <!-- end banner -->
+    <!-- weare -->
+    <div class="weare">
+      <div class="container">
+        <div class="col">
+          <div class="col">
+            <div class="col">
+              <div class="row">
+                <div class="col margin_boo">
+                  <div class="weare_box">
+                    <figure><img src="./assets/images/lala-azizli-phzc9norfsg-unsplash.jpg" alt="#" /></figure>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="weare_box">
+                <h2 class="mt-5">Лучший способ заработать деньги - это зарабатывать деньги</h2>
+                <p><strong></strong> Деньги "приходят" к нам по множеству разных причин. Но главная причина этого - наши повседневные привычки.То, как мы думаем и как ведем себя каждый день, полностью определяет уровень нашего дохода.Итак, каков наилучший способ увеличить свой доход (и ускорить выход из бедности)?Эта статья поможет вам ответить на этот вопрос.У каждого человека есть возможность стать самым богатым человеком в мире (при соблюдении определенных условий).Главное здесь - воспользоваться этим преимуществом!Дело в том, что сегодня богатые люди стали очень богаты благодаря своим умелым повседневным привычкам.Многие из них даже говорят, что они самые счастливые люди на планете. В этой статье мы поговорим о том, как избавиться от финансовых проблем и обрести процветание.1) Измените свое мышление.У каждого человека на этой земле есть возможность стать богатым или бедным. Никто не думает, что он разбогатеет через месяц, или через год, или через год, или через два. Однако если вы поставите перед собой амбициозную цель — создать собственный капитал к концу года (или через пять, 10, или 20-30 лет), то у вас обязательно все получится.Единственное, что вам нужно сделать, это принять правильное шаги для достижения ваших целей.Хорошая стратегия - потратить время на то, чтобы полностью понять свои финансовые цели и то, как вы можете значительно улучшить свое финансовое положение. 2) Начните инвестировать.Заставьте ваши деньги работать на вас.Поставьте перед собой цель: вкладывать 10-20% от каждого получаемого вами дохода.Для этого регулярно покупайте валюту, акции и другие финансовые инструменты.Это позволит вам постепенно сформировать внушительный капитал, доход от которого полностью обеспечит вас и всю вашу семью. 3) Сократите свои потери по кредитам.Никогда не берите взаймы. Точка. Любой долг "вреден" для вашей жизни, ваших отношений с другими людьми. люди и ваши шансы разбогатеть. Дело в том, что если у вас есть кредит, то вы никогда не станете богатым (и даже если вы разбогатеете, это произойдет не под эгидой Банка).4) Делайте разумные инвестиции.Строго придерживайтесь правила: храните только наличные, банковские карты и инвестиции (это самый надежный способ вложения денег, потому что он очень волатилен). 5) заботьтесь о своем здоровье.Сдайте анализы (как хороший вариант, ежегодное бесплатное медицинское обследование). 6) упражняйтесь с умом.Регулярно повышайте уровень своей физической подготовки, заботясь о своем здоровье (физические упражнения, диета, сон и общение). 7) сделать Умные деньги.Заведите хорошую привычку инвестировать 10-20% от каждого заработанного вами дохода (регулярно покупая валюту, акции и другие финансовые инструменты).Это позволит вам постепенно сформировать внушительный капитал, доход от которого полностью обеспечит вас и всю вашу семью. 1</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- weare -->
+
+    <!--  footer -->
+    <footer>
+      <div class="footer">
+        <div class="copyright">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <p>
+                  ©
+                  <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+                  Copyright
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- end footer -->
+    <div class="cookie-banner">
+      <p style="color: #000000">
+        Сайт использует файлы cookie. Они позволяют узнавать вас и получать информацию о вашем пользовательском опыте.Продолжая просмотр сайта, я соглашаюсь с использованием файлов cookie владельцем сайта в соответствии с
+        <a target="_blank" href="https://en.wikipedia.org/wiki/HTTP_cookie"
+          >Политикой cookie</a
+        >
+      </p>
+
+      <button class="close-cookie">×</button>
+    </div>
+
+    <script>
+      window.onload = function () {
+        $('.close-cookie').click(function () {
+          $('.cookie-banner').fadeOut();
+        });
+      };
+    </script>
+
+    <script>
+      let elems = document.querySelectorAll('.server-name');
+      elems.forEach((elem) => {
+        elem.innerHTML = window.location.hostname;
+      });
+    </script>
+    <!-- Javascript files-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery-3.0.0.min.js"></script>
+    <script src="js/plugin.js"></script>
+    <!-- sidebar -->
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+  </body>
+</html>
---
+++ php/0u1LZq8DiCBx29sOVOdE0y21D.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+    <!-- site metas -->
+     
+    <title>Статья</title>
+
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css" />
+    <!-- Responsive-->
+    <link rel="stylesheet" href="css/responsive.css" />
+    <!-- fevicon -->
+    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css" />
+    <!-- Tweaks for older IEs-->
+    <link
+      rel="stylesheet"
+      href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
+      media="screen"
+    />
+  </head>
+  <!-- body -->
+  <body class="main-layout">
+    <!-- loader  -->
+    <div class="loader_bg">
+      <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+    </div>
+    <!-- end loader -->
+    <!-- header -->
+    <header>
+      <!-- header inner -->
+      <div class="head_top">
+        <div class="container">
+          <div class="header">
+            <div class="row" style="align-items: center">
+              <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                <div class="full">
+                  <div class="center-desk">
+                    <div class="logo">
+                      <a href="index.php">О бизнесе</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                <nav class="navigation navbar navbar-expand-md navbar-dark">
+                  <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarsExample04"
+                    aria-controls="navbarsExample04"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarsExample04">
+                    <ul class="navbar-nav mr-auto" style="flex-wrap: wrap">
+                      <li class="nav-item">
+                        <a class="nav-link" href="index.php">Главная</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="blog.php">Статьи</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Контакты</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="policy.php">Политика конфиденциальности</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="terms.php">Условия и положения</a>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end header inner -->
+        <!-- end header -->
+        <!-- banner -->
+        <section class="banner_main">
+          <div class="container">
+            <div class="row d_flex">
+              <div class="col-md-12">
+                <div class="text-bg">
+                  <h1>Статья</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </header>
+    <!-- end banner -->
+    <!-- weare -->
+    <div class="weare">
+      <div class="container">
+        <div class="col">
+          <div class="col">
+            <div class="col">
+              <div class="row">
+                <div class="col margin_boo">
+                  <div class="weare_box">
+                    <figure><img src="./assets/images/moran-yh8v3zlcq5u-unsplash.jpg" alt="#" /></figure>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="weare_box">
+                <h2 class="mt-5">5 планов действий, которые сделают вас богатым</h2>
+                <p><strong></strong> Я уверен, что почти в каждом доме есть хотя бы один-два таких плана.Хотя, возможно, вам стоит переосмыслить свой "заранее накопленный" опыт: 1) Переехать на новое место работы (и совершенно другой круг общения).Наверняка у вас есть опыт работы в одной из социальных сетей, что приведет к совершенно новому уровню дохода. Откройте для себя вики.2) Сделайте так, чтобы ваши деньги "работали" на вас.Это, наверное, самый простой и важный шаг.Только это позволит вам постепенно создавать внушительный финансовый капитал, доход от которого который будет полностью содержать всю вашу семью.3) Заведите хорошую привычку откладывать 10-20% от каждой зарплаты, которую вы получаете.Это позволит вам постепенно создать внушительную сумму капитала, доход от которого будет полностью обеспечивать вас и всю вашу семью.4) Начните инвестировать Ваши деньги должны приносить вам не только пассивный доход, но и "источник дохода".Регулярно покупайте валюту, акции и другие финансовые инструменты.Это позволит вам постепенно сформировать внушительный капитал, доход от которого полностью обеспечит вас и всю вашу семью.5) Займитесь пятым пунктом.Это будет примите форму стратегии ежемесячного усреднения.Так как чем раньше вы начнете, тем более продвинутые функции вы сможете реализовать. семья.1) Прочтите мою статью "Куда НЕ СТОИТ вкладывать деньги? ТОП-3 самых опасных мест для денег". 2) Прочтите мою статью " какие навыки вам нужны, чтобы стать богатым".</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- weare -->
+
+    <!--  footer -->
+    <footer>
+      <div class="footer">
+        <div class="copyright">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <p>
+                  ©
+                  <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+                  Copyright
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- end footer -->
+    <div class="cookie-banner">
+      <p style="color: #000000">
+        Сайт использует файлы cookie. Они позволяют узнавать вас и получать информацию о вашем пользовательском опыте.Продолжая просмотр сайта, я соглашаюсь с использованием файлов cookie владельцем сайта в соответствии с
+        <a target="_blank" href="https://en.wikipedia.org/wiki/HTTP_cookie"
+          >Политикой cookie</a
+        >
+      </p>
+
+      <button class="close-cookie">×</button>
+    </div>
+
+    <script>
+      window.onload = function () {
+        $('.close-cookie').click(function () {
+          $('.cookie-banner').fadeOut();
+        });
+      };
+    </script>
+
+    <script>
+      let elems = document.querySelectorAll('.server-name');
+      elems.forEach((elem) => {
+        elem.innerHTML = window.location.hostname;
+      });
+    </script>
+    <!-- Javascript files-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery-3.0.0.min.js"></script>
+    <script src="js/plugin.js"></script>
+    <!-- sidebar -->
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+  </body>
+</html>
